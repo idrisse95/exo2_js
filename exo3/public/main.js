e38716ac6a381a5191f1b978b148e49cfe3e263f
55973 <55973@etu.he2b.be>
2022-12-19 11:46:22 +0100

    exo3

---
+++ exo3/public/main.js
@@ -0,0 +1,7 @@
+let nb1 = prompt('entrez un chiffre')
+let nb2 = prompt('entrez deuxieme cjiffre')
+let sum = nb1 + nb2
+let multi = nb1*nb2
+let div = nb1/nb2
+let dif = nb1-nb2
+alert(sum+" "+multi+" "+dif+" "+div)
